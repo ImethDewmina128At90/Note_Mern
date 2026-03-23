9027210113815f348070fba1e9b7bc32d4343e40
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2026-03-23 21:29:04 +0530

    added env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://imeth128_db_user:s4SXqbOLau1DyCO2@cluster0.moigty1.mongodb.net/notes_db?appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (error) {
         console.error(error);
